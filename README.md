2a55f3265efd16ba0fce3a5e57895598df05ef56
Adam Duke <adam.v.duke@gmail.com>
2011-04-17 11:54:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Purpose:
+--------
+
+ADOAuthorizeiOS is a static library designed to simplify going through the OAuth flow to get an access token. 
+It borrows concepts from Ben Gottlieb's [Twitter-OAuth-iPhone](https://github.com/bengottlieb/Twitter-OAuth-iPhone).
+
+Building/Running:
+----------------
+
+ADOAuthorizeiOS depends on several git submodules.
+The steps to get up and running are:
+     git clone https://github.com/adamvduke/ADOAuthorizeiOS.git
+     cd ADOAuthorizeiOS
+     git submodule init
+     git submodule update
+
+The submodules will be their own git repositories in the directories ADOAuthorizeiOS/External/
---
+++ ViewController/ADOAuthOOBViewController.h
@@ -0,0 +1,45 @@
+/*  ADOAuthOOBViewController.h
+ *  ADOAuthorizeiOS
+ *
+ *  Created by Adam Duke on 1/13/11.
+ *  Copyright 2011 All rights reserved.
+ */
+
+#import <UIKit/UIKit.h>
+
+@class OAConsumer;
+@class OAToken;
+
+@protocol ADOAuthOOBViewControllerDelegate<NSObject>
+
+- (void)authCompletedWithData:(NSString *)authData orError:(NSError *)error;
+
+@end
+
+@interface ADOAuthOOBViewController : UIViewController <UIWebViewDelegate> {
+	UIWebView *webView;
+
+	@private
+	NSURL *requestTokenURL;
+	NSURL *accessTokenURL;
+	NSURL *authorizeURL;
+
+	OAConsumer *consumer;
+	OAToken *requestToken;
+	NSString *verifier;
+
+	BOOL firstLoad;
+	id<ADOAuthOOBViewControllerDelegate> delegate;
+}
+
+@property (nonatomic, retain) UIWebView *webView;
+@property (nonatomic, retain) id<ADOAuthOOBViewControllerDelegate> delegate;
+@property (nonatomic, retain) NSString *verifier;
+
+- (id)initWithConsumerKey:(NSString *)key
+		   consumerSecret:(NSString *)secret
+	requestTokenURLString:(NSString *)requestTokenURLString
+	 accessTokenURLString:(NSString *)accessTokenURLString
+	   authorizeURLString:(NSString *)authorizeURLString
+				 delegate:(id)aDelegate;
+@end
